14963b525013d8f5057deaa00e7f572aa46c3049
harry.gao <1814593561@qq.com>
2022-11-11 07:47:18 +0800

    hello window

---
+++ cmake/hellowindow.cmake
@@ -0,0 +1,9 @@
+
+add_executable(hellowindow
+${GLAD_SOURCE_DIR}glad.c 
+${CMAKE_SOURCE_DIR}/src/hellowindow.cpp)
+
+if (APPLE) 
+    target_link_libraries(hellowindow ${AppLib})
+endif()
+target_link_libraries(hellowindow glfw3)
