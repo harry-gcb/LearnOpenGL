22e4d42d1362fa281126bc5731a96ba418820dc0
harry.gao <1814593561@qq.com>
2022-12-11 22:05:45 +0800

    finished GAMES101 homework1

--- cmake/GAMES101.cmake
+++ cmake/GAMES101.cmake
@@ -1,12 +1,22 @@
 # include
 include_directories($ENV{LIB_3RDPARTY_PATH}/eigen3/include) # eigen3
+include_directories($ENV{LIB_3RDPARTY_PATH}/opencv/include) # opencv
 include_directories(${CMAKE_SOURCE_DIR}/include) # LearnOpenGL
+
+# lib
+link_directories($ENV{LIB_3RDPARTY_PATH}/opencv/lib)
 
 # set root path
 set(GAMES101_DIR ${CMAKE_SOURCE_DIR}/GAMES101)
 
 # Transformation
-add_executable(Transformation
-${GAMES101_DIR}/Transformation/main.cpp)
+aux_source_directory(${GAMES101_DIR}/Transformation Transformation)
+add_executable(Transformation ${Transformation})
 target_link_libraries(Transformation)
+
+# Assignment
+aux_source_directory(${GAMES101_DIR}/Assignment Assignment)
+add_executable(Assignment ${Assignment})
+target_link_libraries(Assignment opencv_world460d)
+
 
